8316b56e09282bacfcad70910c5d489fa6723806
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-09-29 10:53:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# Program "Play Time "
+'''
+    0. Introduce yourself and ask for user's name.
+    
+    1. Greet the user by the name.
+
+    2. Ask the user how long in minutes the user plays computer games every week. 
+      (turn minutes into seconds)
+      
+    3.Tell the user how much time the user spends playing computer games in a year.
+      (express in total seconds, minutes and seconds, hours and minutes, days and hours)
+      
+      If you decide to do calculations using weeks per year,
+      do the research on how many weeks are in a year.
+           
+      ****************************************************************
+      
+      Help:
+      
+          Built-in functions you might need to use:
+          
+          input()  - takes information from the user and return a string
+          int()    - turns given data into integer ( when possible)
+          str()    - turns given data into string
+          print()  - prints given data
+          
+          Math operations:
+          
+          //  - interger division
+          %   - remainder
+          *   - multiplication
+          /   - float division
+          
+          
+          Reminders for your calculations:
+          
+          1 minute is 60 seconds
+          1 hour is 60 minutes
+          1 day is 24 hours  
+          1 year is 365 days
+                
+'''
